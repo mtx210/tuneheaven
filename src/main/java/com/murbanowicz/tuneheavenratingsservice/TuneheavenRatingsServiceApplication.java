126ef9dcad0675ae087fbe8cc0e73573b2355f09
Max Urbanowicz <m.urbanowicz1@gmail.com>
2024-06-02 00:58:36 +0200

    project init

---
+++ src/main/java/com/murbanowicz/tuneheavenratingsservice/TuneheavenRatingsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.murbanowicz.tuneheavenratingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneheavenRatingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneheavenRatingsServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/murbanowicz/tuneheavenratingsservice/TuneheavenRatingsServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.murbanowicz.tuneheavenratingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneheavenRatingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
